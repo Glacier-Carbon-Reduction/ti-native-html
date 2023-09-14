1e837af2368af15155d42274d0f32fce321dc39d
Kishan <64187898+jgdkishan@users.noreply.github.com>
2023-09-14 22:38:32 +0530

    pages

---
+++ src/scorm/trackers/bind-ga-hj.js
@@ -0,0 +1,18 @@
+
+// Google tag (gtag.js)
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+  gtag('config', 'G-WP2F2B0M4H');
+
+
+// Hotjar Tracking Code for LXP SCORMs
+(function(h,o,t,j,a,r){
+    h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+    h._hjSettings={hjid:3641016,hjsv:6};
+    a=o.getElementsByTagName('head')[0];
+    r=o.createElement('script');r.async=1;
+    r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+    a.appendChild(r);
+    console.log("Hotjar loaded");
+})(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
