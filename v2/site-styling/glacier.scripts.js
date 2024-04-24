67459cb87e328b3a2924b4777ef9a4ba0e3dab3e
Kishan <64187898+jgdkishan@users.noreply.github.com>
2024-04-24 16:33:21 +0530

    patch: learner name

--- v2/site-styling/glacier.scripts.js
+++ v2/site-styling/glacier.scripts.js
@@ -694,7 +694,7 @@ function generateCertificateCanvas(event, visualProps, conditionId) {
       // Draw the learner's name
       wrapCanvasText({
         ctx,
-        text: learnerName,
+        text: visualProps.learnerName,
         x: canvas.width / 2,
         y: canvas.height / 2.5,
         maxWidth: canvas.width - 200,
